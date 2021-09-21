61486bede4be5ad294c069101980d6245a9d1606
Samill <samill.rivasrd@gmail.com>
2021-09-21 15:31:23 -0400

    Add project files.

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infraestructure.Repositories;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPots()
+        {
+            var posts = new PostRespository().GetPosts();
+            return Ok(posts);
+        }
+
+
+    }
+}
